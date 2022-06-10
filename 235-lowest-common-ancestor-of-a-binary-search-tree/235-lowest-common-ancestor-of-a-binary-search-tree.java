b985a3dc4081afe52473427cea5a888f88d0290c
Apurv Jain <97018481+apurvjain7827@users.noreply.github.com>
2022-06-11 00:17:05 +0530

    Time: 11 ms (10.04%), Space: 50.3 MB (17.50%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,44 +10,18 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        Queue<TreeNode> pst = new LinkedList<>();
-        TreeNode pNode = root;
-        while(pNode != null){
-            if(pNode.val < p.val){
-                pst.add(pNode);
-                pNode = pNode.right;
+        TreeNode curr = root;
+        while(true){
+            if(p.val < curr.val && q.val < curr.val){
+                curr = curr.left;
             }
-            else if(pNode.val > p.val){
-                pst.add(pNode);
-                pNode = pNode.left;
+            else if(p.val > curr.val && q.val > curr.val){
+                curr = curr.right;
             }
             else{
-                pst.add(pNode);
                 break;
             }
         }
-        Queue<TreeNode> qst = new LinkedList<>();
-        TreeNode qNode = root;
-        while(qNode != null){
-            if(qNode.val < q.val){
-                qst.add(qNode);
-                qNode = qNode.right;
-            }
-            else if(qNode.val > q.val){
-                qst.add(qNode);
-                qNode = qNode.left;
-            }
-            else{
-                qst.add(qNode);
-                break;
-            }
-        }
-        TreeNode ans = null;
-        while(pst.peek() == qst.peek()){
-            ans = pst.remove();
-            qst.remove();
-        }
-        
-        return ans;
+        return curr;
     }
 }
