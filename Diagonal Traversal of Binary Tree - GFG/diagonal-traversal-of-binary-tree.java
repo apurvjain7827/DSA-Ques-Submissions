9e480d790e60f7df22ff925f13d78a1b43d06906
Apurv Jain <97018481+apurvjain7827@users.noreply.github.com>
2022-07-11 17:18:19 +0530

    Added solution - LeetHub

---
+++ Diagonal Traversal of Binary Tree - GFG/diagonal-traversal-of-binary-tree.java
@@ -0,0 +1,154 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+//Contributed by Sudarshan Sharma
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t-- > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+    	        Tree g = new Tree();
+			    ArrayList<Integer> diagonalNode = g.diagonal(root);
+			    for(int i = 0 ;i<diagonalNode.size();i++){
+			        System.out.print(diagonalNode.get(i)+ " ");
+			    }
+		        System.out.println();
+	        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/* Node is defined as
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}*/
+class Tree
+{
+    static int md;
+    public void trav(Node node, int dia, HashMap<Integer, ArrayList<Integer>> hm){
+        if(node == null)
+        return;
+        md = Math.max(dia, md);
+        if(!hm.containsKey(dia)){
+            hm.put(dia, new ArrayList<>());
+        }
+        hm.get(dia).add(node.data);
+        trav(node.left, dia+1, hm);
+        trav(node.right, dia, hm);
+    }
+     public ArrayList<Integer> diagonal(Node root)
+      {
+           //add your code here.
+           md = 0;
+           HashMap<Integer, ArrayList<Integer>> hm = new HashMap<>();
+            trav(root, 0, hm);
+           ArrayList<Integer> ans = new ArrayList<>();
+           for(int i=0; i<=md; i++){
+               if(hm.get(i) == null)
+               continue;
+               for(int ele : hm.get(i)){
+                   ans.add(ele);
+               }
+           }
+           return ans;
+      }
+}
