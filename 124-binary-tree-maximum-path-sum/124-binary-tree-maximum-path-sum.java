30b6446c52b9e35ca11e75be285e55a4a7d642fb
Apurv Jain <97018481+apurvjain7827@users.noreply.github.com>
2022-08-01 23:19:32 +0530

    Time: 1 ms (99.66%), Space: 47.5 MB (70.28%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
@@ -18,42 +18,32 @@ class Solution {
     
     public int maxPathSum(TreeNode root) {
         ans = Integer.MIN_VALUE;
-        dfs(root);
+        maxRoot2Node(root);
         return ans;
     }
     
-    public void dfs(TreeNode node){
-        if(node == null)
-            return;
-        int lft = maxRoot2Node(node.left);
-        int ryt = maxRoot2Node(node.right);
-        int sum = node.val + Math.max(lft, 0) + Math.max(ryt, 0);
-        if(sum>ans){
-            ans = sum;
-        }
-        dfs(node.left);
-        dfs(node.right);
-    }
+    // public void dfs(TreeNode node){
+    //     if(node == null)
+    //         return;
+    //     int lft = maxRoot2Node(node.left);
+    //     int ryt = maxRoot2Node(node.right);
+    //     int sum = node.val + Math.max(lft, 0) + Math.max(ryt, 0);
+    //     if(sum>ans){
+    //         ans = sum;
+    //     }
+    //     dfs(node.left);
+    //     dfs(node.right);
+    // }
     
     public int maxRoot2Node(TreeNode node){
         if(node == null){
             return 0;
         }
-        if(node.left != null && node.right != null){
-            int left = maxRoot2Node(node.left);
-            int right = maxRoot2Node(node.right);
-            return node.val + Math.max(Math.max(0, left), Math.max(0, right));
-        }
-        else if(node.left != null){
-            int left = maxRoot2Node(node.left);
-            return node.val + Math.max(0, left);
-        }
-        else if(node.right != null){
-            int right = maxRoot2Node(node.right);
-            return node.val + Math.max(0, right);
-        }
-        else{
-            return node.val;
-        }
+        int left = maxRoot2Node(node.left);
+        int right = maxRoot2Node(node.right);
+        int sum = Math.max(left, 0) + Math.max(right, 0) + node.val;
+        if(sum > ans)
+            ans = sum;
+        return Math.max(Math.max(left, right), 0) + node.val;
     }
 }
