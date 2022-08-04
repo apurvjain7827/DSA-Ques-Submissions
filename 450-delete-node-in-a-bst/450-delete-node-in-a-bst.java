71c3c0f16a7fed2040449e3d035ff8f99ceaf63c
Apurv Jain <97018481+apurvjain7827@users.noreply.github.com>
2022-08-04 20:19:21 +0530

    Time: 0 ms (100.00%), Space: 49.8 MB (44.84%) - LeetHub

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
@@ -15,34 +15,28 @@
  */
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-        if(root == null){
+        // helper(root, key);
+        return helper(root, key);
+    }
+    public TreeNode helper(TreeNode root, int key){
+        if(root == null)
             return null;
-        }
-        if(root.val > key){
-            root.left = deleteNode(root.left, key);
-        }
-        else if(root.val < key){
-            root.right = deleteNode(root.right, key);
-        }
-        else{
-            if(root.left != null && root.right != null){
-                TreeNode node = root.right;
-                while(node.left != null){
-                    node = node.left;
-                }
-                node.left = root.left;
-                return root.right;
-            }
-            else if(root.left == null && root.right == null){
+        if(root.val == key){
+            if(root.left == null && root.right == null)
                 return null;
-            }
-            else if(root.left != null){
-                return root.left;
-            }
-            else{
+            if(root.left == null)
                 return root.right;
+            if(root.right == null)
+                return root.left;
+            TreeNode temp = root.left;
+            while(temp.right != null){
+                temp = temp.right;
             }
+            root.val = temp.val;
+            root.left = helper(root.left, temp.val);
         }
+        root.left = helper(root.left, key);
+        root.right = helper(root.right, key);
         return root;
     }
 }
