a3ab8fabdbee7dd9638b6e38bf9ca23de113b9aa
Apurv Jain <97018481+apurvjain7827@users.noreply.github.com>
2022-09-13 00:28:33 +0530

    Time: 240 ms (16.59%), Space: 43.6 MB (63.37%) - LeetHub

---
+++ 839-similar-string-groups/839-similar-string-groups.java
@@ -0,0 +1,63 @@
+class Solution {
+    public int numSimilarGroups(String[] strs) {
+        parent = new int[strs.length];
+        rank = new int[strs.length];
+        for(int i=0; i<parent.length; i++){
+            parent[i] = i;
+            rank[i] = 0;
+        }
+        
+        
+        for(int i=0; i<strs.length-1; i++){
+            String s1 = strs[i];
+            for(int j=i+1; j<strs.length; j++){
+                String s2 = strs[j];
+                int c = 0;
+                for(int k=0; k<s1.length(); k++){
+                    if(s1.charAt(k) != s2.charAt(k)){
+                        c++;
+                    }
+                }
+                if(c == 2 || c == 0)
+                unionhelper(i, j, strs);
+            }
+        }
+        
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i=0; i<parent.length; i++){
+            int par = find(i);
+            hs.add(par);
+        }
+        
+        return hs.size();
+    }
+    
+    int[] parent;
+    int[] rank;
+    
+    public void unionhelper(int i, int j, String[] strs){
+        int s1l = find(i);
+        int s2l = find(j);
+        
+        if(rank[s1l] > rank[s2l]){
+            parent[s2l] = s1l;
+        }
+        else if(rank[s2l] > rank[s1l]){
+            parent[s1l] = s2l;
+        }
+        else{
+            parent[s1l] = s2l;
+            rank[s2l]++;
+        }
+    }
+    
+    public int find(int x){
+        if(parent[x] == x){
+            return x;
+        }
+        else{
+            parent[x] = find(parent[x]);
+            return parent[x];
+        }
+    }
+}
